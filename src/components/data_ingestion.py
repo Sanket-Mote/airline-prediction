73ec7e91c12a06bb59de80e2af3f3304d1b8690e
Sanket Mote <sanket.m88@live.com>
2023-04-12 09:09:38 +0530

    data ingestion v2

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 import pyodbc
 
-from ..exception import CustomException 
-from ..logger import logging
+from src.exception import CustomException 
+from src.logger import logging
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
